1a0734f58fdb9e23b7b20286c4c9b979467416f2
Gabriel Labariento <gabrielmatthew.labariento@gmail.com>
2024-12-14 18:11:45 +0800

    Finished mouseEnter functionality.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let container = document.querySelector(".container");
+
 function createGrid() {
     for (let i = 0; i < 16; i++){
         let row = document.createElement("div");
@@ -8,6 +9,7 @@ function createGrid() {
         for (let j = 0; j < 16; j++){
             let column = document.createElement("div");
             column.style.border = "1px solid red";
+            column.style.flex = "1";
             column.setAttribute("class", "column")
             row.appendChild(column);            
         }
@@ -16,3 +18,26 @@ function createGrid() {
 }
 
 createGrid();
+
+let rows = document.querySelectorAll(".row");
+rows.forEach((row) => {
+    let squares = row.childNodes;
+    squares.forEach((square) => {
+        square.addEventListener("mouseenter", (e) => {
+            e.target.style.backgroundColor = "black";
+            console.log(e.target);
+            
+        })
+    })
+})
+
+
+
+
+
+
+// squares.forEach((square) => {
+//     square.addEventListener("mouseenter", (e) => {
+//         e.target.style.backgroundColor = "black";
+//     })
+// })
